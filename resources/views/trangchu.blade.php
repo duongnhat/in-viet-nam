6a3dcb0e2444c2a571bdbe9e3a36e564423a2245
duongnhat <truongnhat199613@gmail.com>
2020-12-05 20:05:36 +0700

    trang chu

---
+++ resources/views/trangchu.blade.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="vi">
+<head>
+    <title>Cty In Viet Nam</title>
+    <meta charset="utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
+    <meta name="description" content=""/>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <link href="{{ asset('css/styles.css') }}" rel="stylesheet" type="text/css">
+</head>
+<body>
+<section class="page-head m-y-2">
+    <div class="container-fluid">
+        <div class="row content">
+            <div class="col-sm-3 text-right">
+                <img src="{{ URL::to('/') }}/images/logo.jpg" class="img-rounded" alt="Cinque Terre" width="304">
+                <h1><p>CÔNG TY</p>IN VIỆT NAM</h1>
+                <p>Tài khoản: Admin</p>
+                <p>Taylor Swift</p>
+            </div>
+            <div class="col-sm-9">
+                <div class="ba-no"></div>
+            </div>
+        </div>
+    </div>
+</section>
+<hr>
+<section class="page-container">
+    <div class="container-fluid">
+        <h1>My First Bootstrap Page</h1>
+        <p>Resize this responsive page to see the effect!</p>
+    </div>
+</section>
+
+<footer class="container-fluid jumbotron">
+    <p>Footer Text</p>
+</footer>
+<div class="copyright text-center">
+    <div class="container"><small>Copyright © invietnam.com 2020</small></div>
+</div>
+<script type="text/javascript" src="{{ asset('js/scripts.js') }}"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</body>
+</html>
