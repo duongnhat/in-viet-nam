62a5f1f526f78d57d099e0f14233dc396dee3efd
duongnhat <truongnhat199613@gmail.com>
2020-12-20 10:43:42 +0700

    add background image

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
     <link href="{{ asset('css/styles.css') }}" rel="stylesheet" type="text/css">
 </head>
-<body>
+<body style="background-image: url('{{ asset('images/background.png')}}');">
 <section class="page-head pt-2">
     <div class="container-fluid">
         <div class="row content">
