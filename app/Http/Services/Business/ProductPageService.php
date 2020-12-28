ca5486c45fd4ad1a90157e57e6058f44153de220
wakka.truong.nhat <truong_nhat@lampart-vn.com>
2020-12-28 16:41:46 +0700

    product detail

--- app/Http/Services/Business/ProductPageService.php
+++ app/Http/Services/Business/ProductPageService.php
@@ -6,15 +6,18 @@ namespace App\Http\Services\Business;
 
 use App\Http\Services\MyService;
 use App\models\Folder;
+use App\Repositories\Admin\ImageRepository;
 use App\Repositories\Admin\ProductRepository;
 
 class ProductPageService extends MyService
 {
     private $productRepository;
+    private $imageRepository;
 
-    public function __construct(ProductRepository $productRepository)
+    public function __construct(ProductRepository $productRepository, ImageRepository $imageRepository)
     {
         $this->productRepository = $productRepository;
+        $this->imageRepository = $imageRepository;
     }
 
     public function productByFolderPage($folderId)
@@ -26,6 +29,8 @@ class ProductPageService extends MyService
         }
 
         $list = $this->productRepository->getAllByFolderId($folderId);
-        return view('business.list-product-by-folder')->with(['list' => $list, 'folderFather' => $folderFather]);
+        $listImage = $this->imageRepository->getAllByFolderId($folderId);
+
+        return view('business.list-product-by-folder')->with(['list' => $list, 'listImage' => $listImage, 'folderFather' => $folderFather]);
     }
 }
--- resources/views/business/list-product-by-folder.blade.php
+++ resources/views/business/list-product-by-folder.blade.php
@@ -10,9 +10,71 @@
                     <strong>Sorry!</strong> Chưa có dữ liệu.
                 </div>
             @else
-                <div class="row">
+                <div class="row text-center">
                     @foreach($list as $i => $item)
-                        <h1>{{$item->name}}</h1>-----------
+                        <div class="col-lg-3 col-md-6 mb-4">
+                            <div class="card h-100">
+                                @foreach($listImage as $image)
+                                    @if($image->product_id == $item->id)
+                                        <img class="card-img-top" src="{{ url($image->path . '/thumbnail/' . $image->name_to_store) }}" alt="">
+                                        @break
+                                    @endif
+                                @endforeach
+                                <div class="card-body">
+                                    <h4 class="card-title">{{$item->name}}</h4>
+                                    <p class="card-text">{{$item->summary}}</p>
+                                </div>
+                                <div class="d-flex justify-content-between align-items-center m-2">
+                                    <div class="btn-group">
+                                        <button type="button" class="btn btn-sm btn-outline-secondary" data-toggle="modal" data-target=".bd-example-modal-xl{{$item->id}}">Xem nhanh</button>
+                                        <button type="button" class="btn btn-sm btn-outline-secondary" data-toggle="modal" data-target=".bd-example-modal-xl{{$item->id}}">Xem chi tiết</button>
+                                    </div>
+                                    <small class="text-muted">9 mins</small>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="modal fade bd-example-modal-xl{{$item->id}}" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+                            <div class="modal-dialog modal-xl">
+                                <div class="modal-content">
+                                    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+                                        <ol class="carousel-indicators">
+                                            @foreach($listImage as $image)
+                                                @php
+                                                    $no = 0;
+                                                @endphp
+                                                @if($image->product_id == $item->id)
+                                                    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                                                @endif
+                                            @endforeach
+                                        </ol>
+                                        <div class="carousel-inner">
+
+                                            @foreach($listImage as $image)
+                                                @if($image->product_id == $item->id)
+                                                    <div class="carousel-item">
+                                                        <img class="d-block w-100" src="{{ url($image->path . $image->name_to_store) }}" alt="">
+                                                    </div>
+                                                @endif
+                                            @endforeach
+
+                                            <div class="carousel-item active">
+                                                <img class="d-block w-100" src="..." alt="Third slide">
+                                            </div>
+                                        </div>
+                                        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                            <span class="sr-only">Previous</span>
+                                        </a>
+                                        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                            <span class="sr-only">Next</span>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        @break
                     @endforeach
                 </div>
             @endif
